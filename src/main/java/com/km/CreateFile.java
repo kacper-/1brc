c64c07f8a0fa5baf83db5d21cbf3dfe9ce9e4039
Kacper Marczewski <kacper.marczewski@gmail.com>
2024-02-25 21:16:14 +0100

    quick file read

---
+++ src/main/java/com/km/CreateFile.java
@@ -0,0 +1,36 @@
+package com.km;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.Random;
+
+public class CreateFile {
+    private static final int SIZE = 1000000000;
+    private static final int ID_COUNT = 10000;
+    private static final String SEPARATOR = ";";
+
+    public static void main(String[] args) {
+        String[] ids = new String[ID_COUNT];
+        for (int i = 0; i < ID_COUNT; i++)
+            ids[i] = String.format("id%04d", i);
+
+        File file = new File("./input.txt");
+        try {
+            OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file));
+            int j;
+            float temp;
+            for (int i = 0; i < SIZE; i++) {
+                j = new Random().nextInt(ID_COUNT);
+                temp = new Random().nextInt(999);
+                temp /= 10;
+                if (new Random().nextBoolean())
+                    temp = -temp;
+                writer.write(String.format("%s%s%.1f\n", ids[j], SEPARATOR, temp));
+            }
+            writer.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/com/km/Main.java
+++ src/main/java/com/km/Main.java
@@ -1,7 +1,37 @@
 package com.km;
 
+import java.io.InputStream;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Date;
+
 public class Main {
+    private static InputStream in;
+
     public static void main(String[] args) {
-        System.out.println("Works");
+        try {
+            long start = new Date().getTime();
+            RandomAccessFile file = new RandomAccessFile("./input.txt", "r");
+            FileChannel channel = file.getChannel();
+            ByteBuffer bb = ByteBuffer.allocateDirect(1000000);
+            int i = 0;
+            while (channel.read(bb) > -1) {
+                i++;
+                bb.position(0);
+                System.out.println("read " + i + " position " + channel.position());
+            }
+            channel.close();
+            file.close();
+            long stop = new Date().getTime();
+            System.out.println("number of lines = " + i);
+            System.out.println("execution time = " + (stop - start));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String readString(char c) {
+        return null;
     }
 }
